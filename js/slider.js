028c7f823739181c6e1df229738f81e4bb1e35ae
AleksaDursun <45974884+AleksaDursun@users.noreply.github.com>
2019-03-07 21:22:24 +0100

    Crosfade Slider

--- js/slider.js
+++ js/slider.js
@@ -1,23 +1,4 @@
 
-var slideIndex = 0;
-carousel();
-/* Function whitch controls image change */
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("slide");
-  slideIndex++;
-  if (slideIndex > x.length) 
-  {
-    slideIndex = 1;
-    for (i = 0; i < x.length; i++) {
-      x[i].style.zIndex = "0"; 
-    }
-  } 
-  x[slideIndex-1].style.zIndex = slideIndex; 
-  x[slideIndex-1].style.animation="fade 1000ms linear 0s 1 ";
-  setTimeout(carousel, 5000); 
-}
-
 /* Function whitch shows and hides mobile navigation menu*/
 function Mobile()
 {
@@ -27,4 +8,33 @@ function Mobile()
   } else {
     x.style.display = "block";
   }
+}
+var slideIndex = 0;
+carousel();
+/* Function whitch controls image change */
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slide");
+    for (i = 0; i < x.length; i++) {
+      
+      x[i].style.display = "none";
+      if(slideIndex!=0)
+      {
+        if(i==slideIndex-1)
+        {
+          x[i].style.display = "block";
+          x[i].style.zIndex = "1"; 
+        }
+      }
+      else
+      {
+        x[x.length-1].style.zIndex = "1";
+      }     
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    x[slideIndex-1].style.zIndex = "2";
+    x[slideIndex-1].style.animation="fade 1000ms linear 0s 1 ";
+    setTimeout(carousel, 5000);
 }
