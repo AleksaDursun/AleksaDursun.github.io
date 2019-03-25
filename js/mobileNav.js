a3fd83328ad4df4fee85a14a014628429cc0588b
AleksaDursun <45974884+AleksaDursun@users.noreply.github.com>
2019-03-25 14:45:05 +0100

    changed colors and aded most of the materials

--- js/mobileNav.js
+++ js/mobileNav.js
@@ -1,10 +1,13 @@
 /* Function whitch shows and hides mobile navigation menu*/
 function Mobile()
 {
-    var x = document.getElementById("mobileNavigation");
+  var x = document.getElementById("mobileNavigation");
+    
   if (x.style.display == "block") {
     x.style.display = "none";
+    document.getElementById('headerNav').style.backgroundColor="transparent";
   } else {
     x.style.display = "block";
+    document.getElementById('headerNav').style.backgroundColor="rgba(238,49,36,.8)";
   }
 }
---
+++ js/nav.js
@@ -0,0 +1,31 @@
+window.onscroll=function()
+{
+    if(document.getElementById('mobileNavigation').style.display=="block")
+    {
+        document.getElementById('headerNav').style.backgroundColor='rgba(238,49,36,.8)';
+    }
+    else
+    {
+        if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250)
+        {
+            document.getElementById('headerNav').style.backgroundColor='rgba(238,49,36,.8)';
+            var x=document.getElementsByClassName('regularNav');
+            for(var i=0;i<x.length;i++)
+            {
+                x[i].style.color="#fff";
+            }
+        }
+        else
+        {
+    
+            document.getElementById('headerNav').style.backgroundColor='transparent';
+            var x=document.getElementsByClassName('regularNav');
+            for(var i=0;i<x.length;i++)
+            {
+                x[i].style.color="#000";
+            }
+        }    
+    }
+   
+    
+}
